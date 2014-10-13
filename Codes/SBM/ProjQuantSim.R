ec0d61edbf495518c97fc301df17068c314b140a
shubhobm <zoom.subha@gmail.com>
2014-10-13 00:04:51 -0500

    Daily automatic update

--- Codes/SBM/ProjQuantSim.R
+++ Codes/SBM/ProjQuantSim.R
@@ -1,11 +1,11 @@
 rm(list=ls())
 set.seed(10092014)
-n<-10000;
 
 library(inline)
 library(Rcpp)
 library(RcppArmadillo)
 library(RcppEigen)
+library(ellipse)
 
 ## cpp source file
 sourceCpp("ProjQuantile.cpp", verbose=TRUE, rebuild=FALSE);
@@ -26,42 +26,58 @@ my.mvrnorm = function(n, mu, Sigma){
   return(sample.matrix)
 }
 
+## function for PQ and WPQ plots for given data
+PwPplot = function(dat, isEllipse=F, sig=NULL, t1, t2){
+  
+  par(mfrow=c(1,2))
+  plot(dat, pch=19, cex=.1, col=adjustcolor("black", alpha.f=.1),
+       main=t1, xlab="", ylab="")
+  
+  for(i in 5:9){
+    iq = c(0,i/10)
+    dat.pq = ProjQuant(dat, iq, 1000)
+    lines(dat.pq[,1], dat.pq[,2], lty=2, col="red")
+  }
+  
+  # WPQ
+  plot(dat, pch=19, cex=.1, col=adjustcolor("black", alpha.f=.1),
+       main=t2, xlab="", ylab="")
+  for(i in 5:9){
+    iq = c(0,i/10)
+    dat.wpq = WtProjQuantProfile(dat, iq, 1000, 100, 0.5, 0.1, 0.9)
+    if(isEllipse)
+      lines(ellipse(sig, level=i/10), col="blue")
+    lines(dat.wpq[,1], dat.wpq[,2], lty=2, col="red")
+  }
+  par(mfrow=c(1,1))
+}
+
 n = 1e4
 ## Simulating from Bivariate normal, mean (0,0), Sigma = ((1 .5), (.5 1))
 sig = matrix(c(1,.5,.5,1), nrow=2)
-X1 = my.mvrnorm(n, mu=c(0,0), Sigma=sig)
-X1.pq = ProjQuant(X1, c(0,.9), 1000)
-X1.wpq = WtProjQuantProfile(X1, c(0,.9), 1000, 100, 0.5, 0.2, 0.9)
-
-
-plot(X1, pch=19, cex=.1, col=adjustcolor("black", alpha.f=.3))
-# normal confidence ellipsoid
-require(ellipse); lines(ellipse(sig, level=.9), col="red", lwd=2, lty=2)
-lines(X1.pq[,1], X1.pq[,2], lwd=2, col="blue")
-lines(X1.wpq[,1], X1.wpq[,2], lwd=2, col="darkgreen")
-
+X1 = my.mvrnorm(5e3, mu=c(0,0), Sigma=sig)
 
 ## Mixture normal simulation
 sig2 = matrix(c(1,-.5,-.5,1), nrow=2)
-X21 = my.mvrnorm(5e3, mu=c(2,0), Sigma=sig)
-X22 = my.mvrnorm(5e3, mu=c(-2,0), Sigma=sig2)
+X21 = my.mvrnorm(5e3, mu=c(1,0), Sigma=sig)
+X22 = my.mvrnorm(5e3, mu=c(-1,0), Sigma=sig2)
 X2 = rbind(X21,X22)
-X2.pq = ProjQuant(X2, c(0,.9), 1000)
-X2.wpq = WtProjQuantProfile(X2, c(0,.9), 1000, 100, 0.5, 0.2, 0.9)
-
-plot(X2, pch=19, cex=.1)
-lines(X2.pq[,1], X2.pq[,2], lwd=2, col="blue")
-lines(X2.wpq[,1], X2.wpq[,2], lwd=2, col="darkgreen")
 
 ## Mixture normal, assymmetric
-X31 = my.mvrnorm(5e3, mu=c(2,0), Sigma=sig)
+X31 = my.mvrnorm(5e3, mu=c(1,0), Sigma=sig)
 X32 = my.mvrnorm(2e3, mu=c(-1,0), Sigma=.3*sig2)
 X3 = rbind(X31,X32)
-X3.pq = ProjQuant(X3, c(0,.9), 1000)
 
-X3.wpq = WtProjQuantProfile(X3, c(0,.9), 1000, 100, 0.5, 0.2, .9)
+## bivariate exponential
+require(VineCopula)
+X1 = rexp(n)
+X2 = rexp(n)
+c12 = BiCopSim(n, 1, par=.5)
+X4 = cbind(X1,X2)*c12
 
-plot(X3, pch=19, cex=.1)
-lines(X3.pq[,1], X2.pq[,2], lwd=2, col="blue")
-lines(X3.wpq[,1], X2.wpq[,2], lwd=2, col="darkgreen")
+
+PwPplot(X1, isEllipse = T, sig=sig, t1="a.",t2="b.")
+PwPplot(X2, t1="c.",t2="d.")
+PwPplot(X3, t1="e.",t2="f.")
+PwPplot(X4, t1="g.",t2="h.")
 
